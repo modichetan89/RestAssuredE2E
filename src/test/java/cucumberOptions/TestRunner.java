d27222e107a036671ea41599043307af52e1a2bf
Chetan Modi <modichetan89@gmail.com>
2021-02-09 16:35:03 +0530

    report integration

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -12,7 +12,7 @@ import io.cucumber.junit.CucumberOptions;
 		tags="@Sanity and @Regression and @DeletePlace", // here as per this tag only Delete step definition will get call, but we have hooks which runs before @DeletePlace tag, that hooks will execute Addplace code
 		monochrome=true,
 		dryRun=false,
-		plugin= {"pretty","html:target/cucumber-reports", "json:target/cucumber.json"}
+		plugin= {"pretty","html:target/cucumber-reports", "json:target/jsonReports/cucumber-report.json"}
 		)
 
 public class TestRunner {
