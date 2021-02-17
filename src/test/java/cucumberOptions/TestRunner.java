ff3b68ac6939b881d31010454ea94a0cc74dd711
Chetan Modi <modichetan89@gmail.com>
2021-02-17 19:13:35 +0530

    remove tags from test runner

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
 		features="src/test/java/features",
 		glue={"stepDefinitions"},
-		tags="@Sanity and @Regression and @DeletePlace", // here as per this tag only Delete step definition will get call, but we have hooks which runs before @DeletePlace tag, that hooks will execute Addplace code
+		//tags="@Sanity and @Regression and @DeletePlace", // here as per this tag only Delete step definition will get call, but we have hooks which runs before @DeletePlace tag, that hooks will execute Addplace code
 		monochrome=true,
 		dryRun=false,
 		plugin= {"pretty","html:target/cucumber-reports", "json:target/jsonReports/cucumber-report.json"}
